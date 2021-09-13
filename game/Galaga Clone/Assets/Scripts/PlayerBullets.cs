7aaf66cb44ae6613e752cf5bba535299ffe68213
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2021-09-13 13:28:57 -0400

    Added basic player functionality

---
+++ game/Galaga Clone/Assets/Scripts/PlayerBullets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullets : MonoBehaviour
+{
+    public int speed;
+    private GameObject background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background =  GameObject.Find("Background");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+
+        RectTransform rect = (RectTransform)background.transform;
+        if (transform.position.x > rect.rect.width)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collid with " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
