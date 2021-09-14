5f544925a80df2edbc232485c8e5472bb3d2cd4d
Malidem <75290028+Malidem@users.noreply.github.com>
2021-09-14 11:43:26 -0400

    Revert "Fixed collisions"
    This reverts commit 1415dd07b8e2e822f08e8bdf6face644c032f9d5.

--- game/Galaga Clone/Assets/Scripts/PlayerBullets.cs
+++ game/Galaga Clone/Assets/Scripts/PlayerBullets.cs
@@ -7,11 +7,13 @@ public class PlayerBullets : MonoBehaviour
     public int speed;
     private GameObject background;
 
+    // Start is called before the first frame update
     void Start()
     {
         background =  GameObject.Find("Background");
     }
 
+    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
@@ -23,8 +25,9 @@ public class PlayerBullets : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        print("collid with " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
